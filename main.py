7548f06586e624dfa45fa99ed76a19797e012a0a
daryleen <113789045+daryleen@users.noreply.github.com>
2022-09-17 19:46:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# УВВ-111
+# Федорова Д
+# Практика 1
+# Прямоугольник задается координатами двух его противоположных углов
+# (x1, y1), (x2, y2). Рассчитайте его площадь и периметр.
+# Ввод считается из консоли. Числа x1, y1, x2 и y2 задаются по одному за раз.
+# Выходные данные отображаются на консоли и должны содержать две
+# строки с площадью и периметром. Выводиться должно 2 знака после
+# точки.
+import math
+x1 = float(input("Enter coordinate x1:"))
+y1 = float(input("Enter coordinate x2:"))
+x2 = float(input("Enter coordinate y1:"))
+y2 = float(input("Enter coordinate y2:"))
+side_a = abs(x1 - x2)
+side_b = abs(y1 - y2)
+area = side_a * side_b
+perimeter = 2*(side_a + side_b)
+print("Area: ", "%.2f" % area)
+print("Perimeter: ", "%.2f" % perimeter)
